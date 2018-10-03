068dd0f1c3b3a3930314b13ffcb49f0143626d8f
lehuuthien2 <huuthien268142@gmail.com>
2018-10-03 10:44:42 +0700

    update day 3 - create model, index,... - code web.php

---
+++ resources/views/cats/edit.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 10/02/18
+ * Time: 9:12 CH
+ */
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 09/29/18
+ * Time: 9:20 CH
+ */
